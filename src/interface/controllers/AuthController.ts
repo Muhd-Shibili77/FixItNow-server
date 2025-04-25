2e44bb9f73f4205f46ba6f284fbff9ef9258d0a5
shibili888 <shibilishanu888@gmail.com>
2025-04-25 17:29:13 +0530

    refresh token

--- src/interface/controllers/AuthController.ts
+++ src/interface/controllers/AuthController.ts
@@ -149,7 +149,7 @@ export class AuthController {
 
       res.cookie("refreshToken", response.refreshToken, {
         httpOnly: true,
-        // secure:true,
+        secure:true,
         sameSite: "strict",
         path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
